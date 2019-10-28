e9325f83cb1db0de3fa54f7c37a1fccf96287194
kegman <8837066+kegman@users.noreply.github.com>
2019-10-27 20:30:54 -0400

    Fix: add pump_on to create_brake_command`

--- selfdrive/car/honda/carcontroller.py
+++ selfdrive/car/honda/carcontroller.py
@@ -179,8 +179,7 @@ class CarController():
         #Clarity
         idx = (frame / 2) % 4
         #pump_on, self.last_pump_ts = brake_pump_hysteresis(apply_brake, self.apply_brake_last, self.last_pump_ts, ts)
-        can_sends.extend(hondacan.create_brake_command(self.packer, apply_brake,
-          pcm_override, pcm_cancel_cmd, hud.fcw, idx, CS.CP.carFingerprint, CS.CP.isPandaBlack))
+        can_sends.extend(hondacan.create_brake_command(self.packer, apply_brake, pump_on, pcm_override, pcm_cancel_cmd, hud.fcw, idx, CS.CP.carFingerprint, CS.CP.isPandaBlack))
         #self.apply_brake_last = apply_brake
 
         if CS.CP.enableGasInterceptor:
